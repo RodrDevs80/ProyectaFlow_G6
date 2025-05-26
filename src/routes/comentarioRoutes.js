c477bfdaad4bda60096571203a971485c224bd49
RodrDevs80 <rodr26707@gmail.com>
2025-05-26 20:53:26 -0300

    solucion de problema en routes: comentarios-subtarea

--- src/routes/comentarioRoutes.js
+++ src/routes/comentarioRoutes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { getAllComentarios,getComentarioById,deleteComentarioById,updateComentarioById,createComentario } from "../controllers/comentarioControllers.js";
+import { getAllComentarios, getComentarioById, deleteComentarioById, updateComentarioById, createComentario } from "../controllers/comentarioControllers.js";
 
-const comentarioRouter=Router()
+const comentarioRouter = Router()
 
-comentarioRouter.get('/comentarios',getAllComentarios())
-comentarioRouter.get('/comentario:id',getComentarioById())
-comentarioRouter.delete('/comentario:id',deleteComentarioById())
-comentarioRouter.post('/comentario',createComentario())
-comentarioRouter.put('/comentario:id',updateComentarioById())
+comentarioRouter.get('/comentarios', getAllComentarios)
+comentarioRouter.get('/comentario:id', getComentarioById)
+comentarioRouter.delete('/comentario:id', deleteComentarioById)
+comentarioRouter.post('/comentario', createComentario)
+comentarioRouter.put('/comentario:id', updateComentarioById)
 
 export default comentarioRouter
--- src/routes/subtareaRuotes.js
+++ src/routes/subtareaRuotes.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
-import { createSubtarea,getSubtareaById,getAllSubtareas,deleteSubtareaById,updateSubtareaById} from "../controllers/subtareaControllers.js";
+import { createSubtarea, getSubtareaById, getAllSubtareas, deleteSubtareaById, updateSubtareaById } from "../controllers/subtareaControllers.js";
 
-const subTareaRouter=Router()
+const subTareaRouter = Router()
 
 
-subTareaRouter.get('/subtareas',getAllSubtareas())
-subTareaRouter.get('/subtarea:id',getSubtareaById())
-subTareaRouter.post('/subtarea',createSubtarea())
-subTareaRouter.put('/subtarea:id',deleteSubtareaById())
-subTareaRouter.delete('/subtara:id',updateSubtareaById())
+subTareaRouter.get('/subtareas', getAllSubtareas)
+subTareaRouter.get('/subtarea:id', getSubtareaById)
+subTareaRouter.post('/subtarea', createSubtarea)
+subTareaRouter.put('/subtarea:id', deleteSubtareaById)
+subTareaRouter.delete('/subtara:id', updateSubtareaById)
 
 export default subTareaRouter
